650296c97ef17c506ed3ad32cdf4a2e2246e695a
sism1s <marko.lazic880@gmail.com>
2021-02-04 01:04:32 +0000

    update

--- src/Die.js
+++ src/Die.js
@@ -4,6 +4,7 @@ import "./Die.css";
 class Die extends Component {
   static defaultProps = {
     numberWords: ["one", "two", "three", "four", "five", "six"],
+    val: 5,
   };
 
   constructor(props) {
@@ -14,10 +15,11 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
-    const { numberWords, locked, val, disabled } = this.props;
+    const { numberWords, locked, val, disabled, rolling } = this.props;
 
     let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
     if (locked) classes += "Die-locked";
+    if (rolling) classes += "Die-rolling";
     return (
       <i className={classes} onClick={this.handleClick} disabled={disabled} />
     );
